987f4240fc48b894d4010a08fa432ad1e2320c51
JClark <jclarkgaming01@gmail.com>
2022-02-23 09:53:47 -0500

    Added some stuff

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # custom-mdt
+
+## Planned Features
+ - MDT inside Vehicles
+ - MDT at Fixed Locations like Police Stations
+
+
+
+
+
+ 
+## References Used
+ - Lionh34rt Public Release Scripts for Reference
+    * Check out his [Discord] discord.gg/AWyTUEnGeN
---
+++ config.lua
@@ -0,0 +1,13 @@
+-- Random Coordinates Not accurate
+-- Config.Locations is suppose to be locations you can open the MDT at (Fixed Locations)
+Config.Locations = {
+    ["police1"] = {
+        coords = {x = 523, y = 125, z = 37.07, h = 310, r = 1.0}
+    }
+}
+-- Config.Vehicles are for vehicles that can use the MDT/Opened while inside
+Config.Vehicles = {
+
+}
+
+-- Mobile Device MDT? Like a carryable tablet/phone (Blackberry) etc
