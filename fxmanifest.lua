6488ada32459416e5d9d4b767f5fc6089b0ab9cf
JClark <jclarkgaming01@gmail.com>
2022-02-23 09:48:54 -0500

    fxmanifest File

---
+++ fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'johnclark96'
+description 'A Custom MDT Script'
+version '1.0.0'
+
+ui_page 'html/index.html'
+
+client_script 'client/main.lua'
+
+server_script 'client/main.lua'
+
+files {
+    'html/index.html'
+}
+
+lua54 'yes'
